8bc34f69b34ab01c92ee5a0ea629429266babe55
Elias Stengel-Eskin <esteng@gouda.linguistics.mcgill.ca>
2018-01-24 11:51:56 -0500

    inference engine start

--- code/noisy_elias.py
+++ code/noisy_elias.py
@@ -12,8 +12,9 @@ class Element(object):
         ins_top_prob: probability of inserting the top-level phone (scalar)
         contribs: dict of the contributions of the cells at i-1, j-1, and diagonal to this cell
     """
-    def __init__(self, num_plus):
+    def __init__(self, num_plus, top):
         super(Element, self).__init__()
+        self.top_plu = top
         self.sub_prob = np.zeros((num_plus))
         self.ins_bot_prob = np.zeros((num_plus))
         self.ins_top_prob = 0
@@ -27,7 +28,7 @@ class Element(object):
 
 class Matrix(object):
     """A Matrix stores/aggregates the pseudocounts for each operation"""
-    def __init__(self, top_string, bottom_alphabet, likelihoods, max_edit):
+    def __init__(self, top_string, bottom_alphabet, likelihoods, max_edit, prob_dict):
         super(Matrix, self).__init__()
         self.top_string = top_string
         # self.bottom_string = bottom_string  
@@ -41,20 +42,14 @@ class Matrix(object):
         # self.num_plus = len(self.obs)
         self.num_plus = 3
         # initialize empty charts with |bottom| rows and |top| columns
-        self.full_chart = [[Element(self.num_plus)for i in range(len(self.top_string) + 1)] for j in range(self.bottom_string_length + 1)]
+        self.full_chart = [[Element(self.num_plus, None)for i in range(len(self.top_string) + 1)] for j in range(self.bottom_string_length + 1)]
 
-        self.forward_chart = [[Element(self.num_plus)for i in range(len(self.top_string) + 1)] for j in range(self.bottom_string_length + 1)]
-        self.backward_chart = [[Element(self.num_plus)for i in range(len(self.top_string) + 1)] for j in range(self.bottom_string_length + 1)]
+        self.forward_chart = [[Element(self.num_plus, None)for i in range(len(self.top_string) + 1)] for j in range(self.bottom_string_length + 1)]
+        self.backward_chart = [[Element(self.num_plus, None)for i in range(len(self.top_string) + 1)] for j in range(self.bottom_string_length + 1)]
 
-       # define dirichlet over all edits
-        self.operation_pseudocounts = np.ones((3))/3
-        self.operation_sufficient_stats = np.zeros((3))
-
-        self.ins_bot_pseudocounts = np.ones((self.num_plus))/self.num_plus
-        self.ins_bot_sufficient_stats = np.zeros((self.num_plus))
-
-        self.sub_pseudocounts = np.ones((self.num_top, self.num_plus))
-        self.sub_sufficient_stats = np.zeros((self.num_top, self.num_plus))
+        self.operation_pseudocounts = prob_dict["ops"]
+        self.ins_bot_pseudocounts = prob_dict["ib"]
+        self.sub_pseudocounts = prob_dict["sub"]
 
         # initialize first one (doesn't matter how it's initialized since it's P(aligning nothing w/ nothing))
         self.forward_chart[0][0].ins_top_prob = 1
@@ -76,6 +71,11 @@ class Matrix(object):
         for j in range(len(self.top_string)-1, -1, -1):
             prev_sum = self.backward_chart[-1][j+1].cell_sum()
             self.backward_chart[-1][j].ins_top_prob += prev_sum*self.operation_pseudocounts[0]
+
+        # fill in the top string:
+        for j in range(1, len(self.top_string) + 1):
+            for i in range(len(self.full_chart[0])):
+                self.full_chart[i][j].top_plu = self.top_string[i-1]
 
         # mappings to go from plu/character to index in each list (e.g. the sub list)
         self.plu_to_idx = {x:i for i, x in enumerate(sorted(self.obs))}
@@ -122,6 +122,7 @@ class Matrix(object):
                                                          self.operation_pseudocounts[0]
                     self.forward_chart[i][j].ins_top_prob = to_add
                     self.forward_chart[i][j].contribs['j-1'] = to_add
+                    self.full_chart[i][j].top_plu = self.top_string[j-1]
                   
                 if i > 0 and j > 0:
                     # substitution
@@ -131,6 +132,7 @@ class Matrix(object):
                                                             self.likelihoods[i-1]
                     self.forward_chart[i][j].sub_prob = to_add
                     self.forward_chart[i][j].contribs['i-1,j-1'] = np.sum(to_add)
+                    self.full_chart[i][j].top_plu = self.top_string[j-1]
                     
     def backward(self):
         for i in range(self.bottom_string_length-1, -1, -1):
@@ -165,7 +167,7 @@ class Matrix(object):
         # combine forward and backward to get expected counts of each operation
         self.forward()
         self.backward()
-        self.matrix_product(self.forward_chart, self.backward_chart)
+        return self.matrix_product(self.forward_chart, self.backward_chart)
 
     def matrix_product(self, forward, backward):
         # helper function to multiply charts 
@@ -174,6 +176,8 @@ class Matrix(object):
                 self.full_chart[row_idx][col_idx].ins_bot_prob = self.forward_chart[row_idx][col_idx].ins_bot_prob * self.backward_chart[row_idx][col_idx].ins_bot_prob
                 self.full_chart[row_idx][col_idx].sub_prob = self.forward_chart[row_idx][col_idx].sub_prob * self.backward_chart[row_idx][col_idx].sub_prob
                 self.full_chart[row_idx][col_idx].ins_top_prob = self.forward_chart[row_idx][col_idx].ins_top_prob * self.backward_chart[row_idx][col_idx].ins_top_prob
+
+        return self.full_chart
 
     def decode(self): 
         # find path of max contribution 
@@ -206,6 +210,63 @@ class Matrix(object):
 
         output_str.reverse()
         return output_str
+
+class InferenceEngine(object):
+    """docstring for InferenceEngine"""
+    def __init__(self, top_strings, bottom_strings, likelihoods):
+        super(InferenceEngine, self).__init__()
+        self.top_strings = top_strings
+        self.bottom_strings = bottom_strings
+        self.likelihoods = likelihoods
+        self.bottom_alphabet = set([x for string in bottom_strings for x in string])
+        self.top_alphabet = set([x for string in top_strings for x in string])
+
+
+        self.num_top = len(self.top_alphabet)
+        self.num_plus = len(self.bottom_alphabet)
+
+        # define dirichlet over all edits
+        self.operation_pseudocounts = np.ones((3))/3
+        self.operation_suff_stats = np.zeros((3))
+
+        self.ins_bot_pseudocounts = np.ones((self.num_plus))/self.num_plus
+        self.ins_bot_suff_stats = np.zeros((self.num_plus))
+
+        self.sub_pseudocounts = np.ones((self.num_top, self.num_plus))
+        self.sub_suff_stats = np.zeros((self.num_top, self.num_plus))
+
+
+    def learn(self):
+        prob_dict = {'ops': self.operation_pseudocounts, 'ib': self.ins_bot_pseudocounts, 'sub': self.sub_pseudocounts}
+        for t,b,l in zip(self.top_strings, self.bottom_strings, self.likelihoods):
+
+            m = Matrix(t,self.bottom_alphabet,l,2, prob_dict)
+            print(t)
+            print(b)
+            self.acc_sufficient_stats(m)
+
+
+    def acc_sufficient_stats(self, m):
+        chart = m.forward_backward()
+        print(m.decode())
+        pprint_chart(chart)
+
+        self.operation_suff_stats += m.operation_pseudocounts
+
+        for i in range(1, len(chart)):
+            for j in range(1, len(chart[0])):
+                element = chart[i][j]
+                top_plu_idx = m.top_to_idx[element.top_plu]
+
+                self.ins_bot_suff_stats += element.ins_bot_prob
+                self.sub_suff_stats[top_plu_idx] += element.sub_prob
+
+    def local_updates(self):
+        pass
+    def global_updates(self):
+        pass
+
+
 
 def pprint_chart(chart):
     list_form = [[0 for i in range(len(chart[0]))] for j in range(len(chart))]
@@ -241,13 +302,14 @@ if __name__ == '__main__':
 
     top_strings, bottom_strings, likelihoods = generate_data()
 
+    inf_eng = InferenceEngine(top_strings, bottom_strings, likelihoods)
+    inf_eng.learn()
+    # bottom_alphabet = set([x for string in bottom_strings for x in string])
 
-    bottom_alphabet = set([x for string in bottom_strings for x in string])
-
-    for t,b,l in zip(top_strings, bottom_strings, likelihoods):
-        m = Matrix(t,bottom_alphabet,l,2)
-        # have it return pseudocounts 
-        full_chart = m.forward_backward()
+    # for t,b,l in zip(top_strings, bottom_strings, likelihoods):
+    #     m = Matrix(t,bottom_alphabet,l,2)
+    #     # have it return pseudocounts 
+    #     full_chart = m.forward_backward()
 
     print('done')
 
