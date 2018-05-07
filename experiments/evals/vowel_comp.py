77fd4bad67b9515e9182141010e709c7b5029650
esteng <elias.stengel@gmail.com>
2018-05-06 20:59:13 -0400

    refactor getting vowel formants

--- experiments/evals/vowel_comp.py
+++ experiments/evals/vowel_comp.py
@@ -21,9 +21,13 @@ from pyraat.parse_outputs import parse_point_script_output
 # average formants over vowels, plot on vowel chart 
 
 
+# problem: how to determine which segments in the aligned are vowels
+# solution 1: get formants for all segments, based on formants decide which should be considered vowels
+
+
 vowel_gex = re.compile(r"[aeiou].*")
 
-def get_reference_vowels(tg_path, audio_path):
+def get_vowel_formants(tg_path, audio_path, reference=True):
 	"""
 	read a gold-standard textgrid, return all vowel intervals 
 	"""
@@ -53,42 +57,44 @@ def get_reference_vowels(tg_path, audio_path):
 	output = analyze_segments(vowels, formant_function) 
 	return output
 
+def plot(ref_vowels, found_vowels):
+	small_d = dict()
+	# d = list(ref_vowels.items())[0][0]
+	for key, value in ref_vowels.items():
+		label = key.__dict__["properties"]['label']
+		f1 = value["F1"]
+		f2 = value["F2"]
+		try: 
+			small_d[label].append([f1, f2])
+		except KeyError:
+			small_d[label] = [[f1, f2]]
 
-small_d = dict()
-ref_vowels = get_reference_vowels("/Users/Elias/ULD/audio/icicles/icicles.TextGrid", "/Users/Elias/ULD/audio/icicles/icicles.wav")
-# d = list(ref_vowels.items())[0][0]
-for key, value in ref_vowels.items():
-	label = key.__dict__["properties"]['label']
-	f1 = value["F1"]
-	f2 = value["F2"]
-	try: 
-		small_d[label].append([f1, f2])
-	except KeyError:
-		small_d[label] = [[f1, f2]]
+	# average
+	for key,value in small_d.items():
+		value = np.array(value)
+		avg_value = np.sum(value, axis=0)/value.shape[0]
+		small_d[key] = avg_value
 
-# average
-for key,value in small_d.items():
-	value = np.array(value)
-	avg_value = np.sum(value, axis=0)/value.shape[0]
-	small_d[key] = avg_value
+	sorted_items = sorted(small_d.items(), key=lambda x: x[0])
+	labels = [x[0] for x in sorted_items]
+	formants = [x[1] for x in sorted_items]
+	f1 = [x[0] for x in formants]
+	f2 = [x[1] for x in formants]
+	# plot formants
+	plt.scatter(f1, f2)
 
-sorted_items = sorted(small_d.items(), key=lambda x: x[0])
-labels = [x[0] for x in sorted_items]
-formants = [x[1] for x in sorted_items]
-f1 = [x[0] for x in formants]
-f2 = [x[1] for x in formants]
-# plot formants
-plt.scatter(f1, f2)
+	for item in sorted_items:
+		label = item[0]
+		f1 = item[1][0]
+		f2 = item[1][1]
+		plt.annotate(label, xy=(f1, f2))
 
-for item in sorted_items:
-	label = item[0]
-	f1 = item[1][0]
-	f2 = item[1][1]
-	plt.annotate(label, xy=(f1, f2))
-
-plt.show()
-
+	plt.show()
 
 
 
+
+if __name__ == '__main__':
+	ref_vowels = get_vowel_formants("/Users/Elias/ULD/audio/icicles/icicles.TextGrid", "/Users/Elias/ULD/audio/icicles/icicles.wav")
+	prod_vowels = get_vowel_formants("/Users/Elias/ULD/audio/icicles/")
 
